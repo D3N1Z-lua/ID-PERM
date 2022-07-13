d4bbf70268a3135f8e47b69cab3a3fe41e130d41
D3N1Z <108899574+D3N1Z-lua@users.noreply.github.com>
2022-07-13 03:01:56 +0200

    ID PERM
    ID Permanent for fivem

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Permanent id system'
+author 'Denizdufix#6767'
+
+client_script 'Client/Client.lua'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+    'Server/Server.lua'
+}
+
+
+
